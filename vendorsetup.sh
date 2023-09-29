ba2365118dc571b436bff5088209a87eff6851f0
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2023-09-29 15:46:01 -0700

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_vivo-V2217-user
+add_lunch_combo twrp_vivo-V2217-userdebug
+add_lunch_combo twrp_vivo-V2217-eng
+#export ALLOW_MISSING_DEPENDENCIES=true
